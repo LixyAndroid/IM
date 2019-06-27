c68110dbcdead4acc8922611534cdb0a3137de4d
lixuyang <958607524@qq.com>
2019-06-27 23:10:04 +0800

    Splash界面View层实现

---
+++ app/src/main/java/com/example/im/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.example.im
+
+/**
+ * @author  Mloong
+ * date  2019/6/27 23:05
+ */
+class LoginActivity: BaseActivity() {
+
+    override fun getLayoutReId(): Int =
+            R.layout.activity_login
+
+
+
+}
--- app/src/main/java/com/example/im/SplashActivity.kt
+++ app/src/main/java/com/example/im/SplashActivity.kt
@@ -1,10 +1,41 @@
 package com.example.im
 
+import android.os.Handler
+import com.example.im.contract.SplashContract
+import org.jetbrains.anko.startActivity
+
 /**
  * @author  Mloong
  * date  2019/6/27 21:47
  */
-class SplashActivity : BaseActivity() {
+class SplashActivity : BaseActivity() ,SplashContract.View{
+
+    companion object{
+        val DELAY = 2000L
+    }
+
+    val handler by lazy {
+        Handler()
+    }
+
+
+    //延时2s,跳转到登录界面
+    override fun onNotLoggedIn() {
+        handler.postDelayed({
+            startActivity<LoginActivity>()
+            finish()
+        }, DELAY)
+
+
+    }
+
+
+    //跳转到主界面
+    override fun onLoggedIn() {
+        startActivity<MainActivity>()
+        finish()
+
+    }
 
     override fun getLayoutReId(): Int =
         R.layout.activity_splash
