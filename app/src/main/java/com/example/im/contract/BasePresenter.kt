6e01addd229cf2d176ffcb375c0a3d349431a7ce
lixuyang <958607524@qq.com>
2019-06-27 23:02:12 +0800

    Splash界面MVP协议

---
+++ app/src/main/java/com/example/im/contract/BasePresenter.kt
@@ -0,0 +1,9 @@
+package com.example.im.contract
+
+/**
+ * @author  Mloong
+ * date  2019/6/27 22:58
+ */
+interface BasePresenter {
+
+}
---
+++ app/src/main/java/com/example/im/contract/SplashContract.kt
@@ -0,0 +1,18 @@
+package com.example.im.contract
+
+/**
+ * @author  Mloong
+ * date  2019/6/27 22:59
+ */
+interface SplashContract {
+
+    interface  Presenter:BasePresenter{
+        fun checkLogin()
+    }
+
+
+    interface  View{
+        fun  onNotLoggedIn() //没有登录的ui处理
+        fun  onLoggedIn() //已经登录的的ui处理
+    }
+}
