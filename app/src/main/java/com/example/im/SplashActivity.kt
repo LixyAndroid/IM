197f645e3772fadebf2441bcba53e6c4457614fc
lixuyang <958607524@qq.com>
2019-06-27 21:52:44 +0800

    Splash界面实现

---
+++ app/src/main/java/com/example/im/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.example.im
+
+/**
+ * @author  Mloong
+ * date  2019/6/27 21:47
+ */
+class SplashActivity : BaseActivity() {
+
+    override fun getLayoutReId(): Int =
+        R.layout.activity_splash
+
+
+}
