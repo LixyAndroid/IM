602cd024e7579b2757316e246dcba292d74909a0
lixuyang <958607524@qq.com>
2019-06-28 22:36:49 +0800

    长按弹出删除好友对话框

--- app/src/main/java/com/example/im/adapter/ContactListAdapter.kt
+++ app/src/main/java/com/example/im/adapter/ContactListAdapter.kt
@@ -1,9 +1,13 @@
 package com.example.im.adapter
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.content.Context
+import android.content.DialogInterface
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.im.R
 import com.example.im.data.ContactListItem
 import com.example.im.ui.activity.ChatActivity
 import com.example.im.widget.ContactListItemView
@@ -32,6 +36,24 @@ class ContactListAdapter(val context: Context, val contactListItems: MutableList
         val  userName = contactListItems[position].userName
 
         contactListItemView.setOnClickListener { context.startActivity<ChatActivity>("username" to userName) }
+
+        contactListItemView.setOnLongClickListener {
+            val message = String.format(context.getString(R.string.delete_friend_message),userName)
+
+            AlertDialog.Builder(context)
+                    .setTitle(R.string.delete_friend_title)
+                    .setMessage(message)
+                    .setNegativeButton(R.string.cancel,null)
+                    .setPositiveButton(R.string.confirm,DialogInterface.OnClickListener { dialogInterface, i ->
+                        deleteFriend(userName)
+                    }).show()
+            //返回值
+                 true
+            }
+    }
+
+    private fun deleteFriend(userName: String) {
+
 
     }
 
