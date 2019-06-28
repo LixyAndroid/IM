0beda301a0eb9f57ab77590265750422758fda63
lixuyang <958607524@qq.com>
2019-06-28 21:09:00 +0800

    联系人界面布局初始化

--- app/src/main/java/com/example/im/ui/fragment/ContactFragment.kt
+++ app/src/main/java/com/example/im/ui/fragment/ContactFragment.kt
@@ -1,6 +1,8 @@
 package com.example.im.ui.fragment
 
+import android.view.View
 import com.example.im.R
+import kotlinx.android.synthetic.main.fragment_contacts.*
 import kotlinx.android.synthetic.main.header.*
 
 /**
@@ -16,6 +18,12 @@ class ContactFragment :BaseFragment(){
         super.init()
 
         headerTitle.text = getString(R.string.contact)
+        add.visibility = View.VISIBLE
+
+        swipeRefreshLayout.apply {
+            setColorSchemeResources(R.color.qq_blue)
+            isRefreshing = true
+        }
     }
 
 }
