0928d205c0d3ce22f9b90f8a9d60b9979edc6d2b
lixuyang <958607524@qq.com>
2019-06-28 21:28:52 +0800

    联系人界面的View层的实现

--- app/src/main/java/com/example/im/ui/fragment/ContactFragment.kt
+++ app/src/main/java/com/example/im/ui/fragment/ContactFragment.kt
@@ -4,15 +4,30 @@ import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.im.R
 import com.example.im.adapter.ContactListAdapter
+import com.example.im.contract.ContactContract
 import kotlinx.android.synthetic.main.fragment_contacts.*
 import kotlinx.android.synthetic.main.header.*
+import org.jetbrains.anko.toast
 
 /**
  * @author  Mloong
  * date  2019/6/28 19:26
  */
 
-class ContactFragment :BaseFragment(){
+class ContactFragment :BaseFragment(),ContactContract.View{
+
+    override fun onLoadContactSuccess() {
+        swipeRefreshLayout.isRefreshing = false
+        recyclerView.adapter?.notifyDataSetChanged()
+
+
+    }
+
+    override fun onLoadContactsFailed() {
+        swipeRefreshLayout.isRefreshing = false
+
+        context?.toast(R.string.load_contacts_failed)
+    }
 
     override fun getLayoutResId(): Int  = R.layout.fragment_contacts
 
