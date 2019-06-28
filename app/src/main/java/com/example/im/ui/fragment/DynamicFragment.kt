e8ce38b6017fc4108517d50b57ff5c173ca765b9
lixuyang <958607524@qq.com>
2019-06-28 20:10:29 +0800

    动态界面布局

--- app/src/main/java/com/example/im/ui/fragment/DynamicFragment.kt
+++ app/src/main/java/com/example/im/ui/fragment/DynamicFragment.kt
@@ -1,6 +1,9 @@
 package com.example.im.ui.fragment
 
 import com.example.im.R
+import com.hyphenate.chat.EMClient
+import kotlinx.android.synthetic.main.fragment_dynamic.*
+import kotlinx.android.synthetic.main.header.*
 
 /**
  * @author  Mloong
@@ -11,6 +14,14 @@ class DynamicFragment :BaseFragment(){
 
     override fun getLayoutResId(): Int  = R.layout.fragment_dynamic
 
+    override fun init() {
+        super.init()
 
+        headerTitle.text = getString(R.string.dynamic)
+
+        val logoutString = String.format(getString(R.string.logout),EMClient.getInstance().currentUser)
+
+        logout.text = logoutString
+    }
 
 }
