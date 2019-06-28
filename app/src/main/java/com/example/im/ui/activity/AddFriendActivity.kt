e5bc7e3ac032890921e18b11ad144f6cf508c766
lixuyang <958607524@qq.com>
2019-06-29 00:22:26 +0800

    添加好友界面View层的实现

--- app/src/main/java/com/example/im/ui/activity/AddFriendActivity.kt
+++ app/src/main/java/com/example/im/ui/activity/AddFriendActivity.kt
@@ -3,14 +3,31 @@ package com.example.im.ui.activity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.im.R
 import com.example.im.adapter.AddFriendListAdapter
+import com.example.im.contract.AddFriendContrat
 import kotlinx.android.synthetic.main.activity_add_friend.*
 import kotlinx.android.synthetic.main.header.*
+import org.jetbrains.anko.toast
 
 /**
  * @author  Mloong
  * date  2019/6/28 23:38
  */
-class AddFriendActivity : BaseActivity() {
+class AddFriendActivity : BaseActivity(),AddFriendContrat.View {
+
+    override fun onSearchSuccess() {
+        dismissProgress()
+        toast(R.string.search_success)
+
+        recyclerView.adapter?.notifyDataSetChanged()
+
+    }
+
+    override fun onSearchFailed() {
+
+        dismissProgress()
+        toast(R.string.search_failed)
+    }
+
     override fun getLayoutReId(): Int  = R.layout.activity_add_friend
 
     override fun init() {
