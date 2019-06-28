f828bb0a814740001f461324f590030c2194f6b3
lixuyang <958607524@qq.com>
2019-06-28 15:41:31 +0800

    注册界面的MVP协议

---
+++ app/src/main/java/com/example/im/contract/RegisterContract.kt
@@ -0,0 +1,23 @@
+package com.example.im.contract
+
+/**
+ * @author  Mloong
+ * date  2019/6/28 15:36
+ */
+interface RegisterContract {
+
+    interface  Presenter : BasePresenter{
+        fun register(userName:String,password:String,confirmPassword:String)
+    }
+
+    interface  View{
+        fun onUserNameError()
+        fun onPasswordError()
+        fun onConfirmPasswordError()
+
+        fun onStratRegister()
+        fun onRegisterSuccess()
+        fun onRegisterFailde()
+
+    }
+}
