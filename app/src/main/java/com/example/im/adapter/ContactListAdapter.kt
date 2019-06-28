4f9fc3aef8dbc8dc1f9de1559fd81704e5498d82
lixuyang <958607524@qq.com>
2019-06-28 22:25:04 +0800

    点击联系人跳转到聊天界面

--- app/src/main/java/com/example/im/adapter/ContactListAdapter.kt
+++ app/src/main/java/com/example/im/adapter/ContactListAdapter.kt
@@ -5,7 +5,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.im.data.ContactListItem
+import com.example.im.ui.activity.ChatActivity
 import com.example.im.widget.ContactListItemView
+import org.jetbrains.anko.startActivity
 
 /**
  * @author  Mloong
@@ -27,6 +29,9 @@ class ContactListAdapter(val context: Context, val contactListItems: MutableList
 
         val contactListItemView = holder.itemView as ContactListItemView
         contactListItemView.bindView(contactListItems[position])
+        val  userName = contactListItems[position].userName
+
+        contactListItemView.setOnClickListener { context.startActivity<ChatActivity>("username" to userName) }
 
     }
 
---
+++ app/src/main/java/com/example/im/ui/activity/ChatActivity.kt
@@ -0,0 +1,13 @@
+package com.example.im.ui.activity
+
+import com.example.im.R
+
+/**
+ * @author  Mloong
+ * date  2019/6/28 22:20
+ */
+class ChatActivity:BaseActivity() {
+    override fun getLayoutReId(): Int = R.layout.activity_chat
+
+
+}
