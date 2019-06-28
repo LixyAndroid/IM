9b3449eeb57903ae0dd9728b84f89e7fd973038b
lixuyang <958607524@qq.com>
2019-06-28 16:58:18 +0800

    注册Bmob

--- app/src/main/java/com/example/im/presenter/RegisterPresenter.kt
+++ app/src/main/java/com/example/im/presenter/RegisterPresenter.kt
@@ -1,8 +1,12 @@
 package com.example.im.presenter
 
+import cn.bmob.v3.BmobUser
+import cn.bmob.v3.exception.BmobException
+import cn.bmob.v3.listener.SaveListener
 import com.example.im.contract.RegisterContract
 import com.example.im.extentions.isValidPassword
 import com.example.im.extentions.isValidUserName
+
 
 /**
  * @author  Mloong
@@ -21,6 +25,8 @@ class RegisterPresenter(val view: RegisterContract.View):RegisterContract.Presen
                 if (password.equals(confirmPassword)){
                     //密码和确认密码一致
                     view.onStratRegister()
+                    //开始注册
+                    registerBmob(userName,password)
 
                 }else view.onConfirmPasswordError()
 
@@ -28,6 +34,28 @@ class RegisterPresenter(val view: RegisterContract.View):RegisterContract.Presen
 
         }else view.onUserNameError()
 
+    }
+
+    /**
+     * 账号密码登录
+     */
+    private fun registerBmob(userName: String,password: String) {
+        val bu = BmobUser()
+        //此处替换为你的用户名
+        bu.username = userName
+        //此处替换为你的密码
+        bu.setPassword(password)
+        bu.signUp<BmobUser>(object : SaveListener<BmobUser>() {
+            override fun done(bmobUser: BmobUser, e: BmobException?) {
+                if (e == null) {
+                    //注册成功
+                    //注册到环信
+                } else {
+                   //注册失败
+                    view.onRegisterFailde()
+                }
+            }
+        })
     }
 
 }
