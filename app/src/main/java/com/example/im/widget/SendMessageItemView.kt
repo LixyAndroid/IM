e192ce0f53af3c801eb2a063a3f0ceb16c623377
lixuyang <958607524@qq.com>
2019-06-29 15:37:01 +0800

    收发信息的布局实现

---
+++ app/src/main/java/com/example/im/widget/SendMessageItemView.kt
@@ -0,0 +1,21 @@
+package com.example.im.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.RelativeLayout
+import com.example.im.R
+import com.example.im.contract.LoginContract
+
+/**
+ * @author  Mloong
+ * date  2019/6/29 15:32
+ */
+class SendMessageItemView(context: Context?, attrs: AttributeSet? = null) : RelativeLayout(context, attrs) {
+
+
+            init {
+                View.inflate(context, R.layout.view_send_message_item,this)
+            }
+
+}
