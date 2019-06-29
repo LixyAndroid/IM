ecbe308409ff2915f4b85cad84250b5276227f4e
lixuyang <958607524@qq.com>
2019-06-29 16:23:07 +0800

    发送一条信息的Model层实现

--- app/src/main/java/com/example/im/presenter/ChatPresenter.kt
+++ app/src/main/java/com/example/im/presenter/ChatPresenter.kt
@@ -1,6 +1,9 @@
 package com.example.im.presenter
 
+import com.example.im.adapter.EMCallBackAdapter
 import com.example.im.contract.ChatContract
+import com.hyphenate.chat.EMClient
+import com.hyphenate.chat.EMMessage
 
 /**
  * @author  Mloong
@@ -8,7 +11,30 @@ import com.example.im.contract.ChatContract
  */
 class ChatPresenter(val view: ChatContract.View):ChatContract.Presenter {
 
+    val messages = mutableListOf<EMMessage>()
+
+
     override fun sendMessage(contrat: String, message: String) {
+
+        //创建一条消息
+        val emMessage = EMMessage.createTxtSendMessage(message,contrat)
+
+        emMessage.setMessageStatusCallback(object : EMCallBackAdapter(){
+
+            override fun onSuccess() {
+                uiThread { view.onSendMessageSuccess() }
+            }
+
+            override fun onError(p0: Int, p1: String?) {
+                uiThread { view.onSendMessageFailed() }
+            }
+
+        })
+
+        messages.add(emMessage)
+        view.onStartSendMessage()
+
+        EMClient.getInstance().chatManager().sendMessage(emMessage)
 
     }
 }
