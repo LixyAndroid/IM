4b80bac6803157aecb3e60af5122acdea3980b3d
lixuyang <958607524@qq.com>
2019-06-29 13:21:25 +0800

    发送添加好友的请求

--- app/src/main/java/com/example/im/app/IMApplication.kt
+++ app/src/main/java/com/example/im/app/IMApplication.kt
@@ -51,8 +51,8 @@ class IMApplication :Application() {
 
         val options = EMOptions()
 
-//    // 默认添加好友时，是不需要验证的，改成需要验证
-//    //        options.setAcceptInvitationAlways(false);
+    // 默认添加好友时，是不需要验证的，改成需要验证
+        options.setAcceptInvitationAlways(false);
 //
 //    // 是否自动将消息附件上传到环信服务器，默认为True是使用环信服务器上传下载，如果设为 false，需要开发者自己处理附件消息的上传和下载
 //            options.setAutoTransferMessageAttachments(true)
--- app/src/main/java/com/example/im/widget/AddFriendListItemView.kt
+++ app/src/main/java/com/example/im/widget/AddFriendListItemView.kt
@@ -5,10 +5,14 @@ import android.util.AttributeSet
 import android.view.View
 import android.widget.RelativeLayout
 import com.example.im.R
+import com.example.im.adapter.EMCallBackAdapter
 import com.example.im.data.AddFriendItem
+import com.hyphenate.chat.EMClient
 import kotlinx.android.synthetic.main.view_add_friend_item.view.*
 import kotlinx.android.synthetic.main.view_contact_item.view.*
 import kotlinx.android.synthetic.main.view_contact_item.view.userName
+import org.jetbrains.anko.runOnUiThread
+import org.jetbrains.anko.toast
 
 /**
  * @author  Mloong
@@ -31,6 +35,27 @@ class AddFriendListItemView(context: Context?, attrs: AttributeSet? = null) : Re
         userName.text = addFriendItem.userName
         timestamp.text = addFriendItem.timestamp
 
+
+        add.setOnClickListener {  addFriend(addFriendItem.userName) }
+
+    }
+
+
+
+    private fun addFriend(userName: String) {
+        EMClient.getInstance().contactManager().aysncAddContact(userName,null,object :EMCallBackAdapter(){
+
+            override fun onSuccess() {
+                context.runOnUiThread { toast(R.string.send_add_friend_success) }
+            }
+
+            override fun onError(p0: Int, p1: String?) {
+                context.runOnUiThread { toast(R.string.send_add_friend_failed) }
+
+
+            }
+
+        })
     }
 
 
