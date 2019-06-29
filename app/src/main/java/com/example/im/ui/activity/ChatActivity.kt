2c61279b5b2da161de9de0f1d345c32018f28e6b
lixuyang <958607524@qq.com>
2019-06-29 15:45:45 +0800

    发送一条信息的View层实现

--- app/src/main/java/com/example/im/ui/activity/ChatActivity.kt
+++ app/src/main/java/com/example/im/ui/activity/ChatActivity.kt
@@ -4,14 +4,39 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import com.example.im.R
+import com.example.im.contract.ChatContract
 import kotlinx.android.synthetic.main.activity_chat.*
 import kotlinx.android.synthetic.main.header.*
+import org.jetbrains.anko.toast
 
 /**
  * @author  Mloong
  * date  2019/6/28 22:20
  */
-class ChatActivity:BaseActivity() {
+class ChatActivity:BaseActivity(),ChatContract.View {
+
+    override fun onStartSendMessage() {
+        //通知RecyclerView刷新列表
+        recyclerView.adapter?.notifyDataSetChanged()
+
+    }
+
+
+    override fun onSendMessageSuccess() {
+        recyclerView.adapter?.notifyDataSetChanged()
+        toast(R.string.send_message_success)
+        //清空编辑框
+        edit.text.clear()
+
+    }
+
+    override fun onSendMessageFailed() {
+        toast(R.string.send_message_failed)
+
+        recyclerView.adapter?.notifyDataSetChanged()
+
+    }
+
     override fun getLayoutReId(): Int = R.layout.activity_chat
 
     override fun init() {
