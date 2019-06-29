aee4cbfa2fc8e7562f3d48524e6407a890433141
lixuyang <958607524@qq.com>
2019-06-29 11:37:30 +0800

    添加好友界面Model层的实现

--- app/src/main/java/com/example/im/presenter/AddFriendPresenter.kt
+++ app/src/main/java/com/example/im/presenter/AddFriendPresenter.kt
@@ -1,7 +1,12 @@
 package com.example.im.presenter
 
+import cn.bmob.v3.BmobQuery
+import cn.bmob.v3.BmobUser
+import cn.bmob.v3.exception.BmobException
+import cn.bmob.v3.listener.FindListener
 import com.example.im.contract.AddFriendContrat
 import com.example.im.ui.activity.AddFriendActivity
+import com.hyphenate.chat.EMClient
 
 /**
  * @author  Mloong
@@ -10,6 +15,18 @@ import com.example.im.ui.activity.AddFriendActivity
 class AddFriendPresenter(val view: AddFriendActivity) : AddFriendContrat.Presenter {
 
     override fun search(key: String) {
+
+        val  query = BmobQuery<BmobUser>()
+        query.addWhereContains("username",key)
+            .addWhereNotEqualTo("username",EMClient.getInstance().currentUser)
+        query.findObjects(object : FindListener<BmobUser>(){
+
+            override fun done(p0: MutableList<BmobUser>?, p1: BmobException?) {
+                if (p1 == null) view.onSearchSuccess()
+                else view.onSearchFailed()
+            }
+
+        })
 
     }
 }
