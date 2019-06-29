74047c05eea9fddab45709d448288875b83aa663
lixuyang <958607524@qq.com>
2019-06-29 13:56:56 +0800

    联系人界面的代码的抽取

--- app/src/main/java/com/example/im/ui/fragment/BaseFragment.kt
+++ app/src/main/java/com/example/im/ui/fragment/BaseFragment.kt
@@ -21,6 +21,7 @@ abstract class BaseFragment:Fragment() {
         init()
 
 
+
     open fun init(){
         //初始化一些公共的功能，比如摇一摇，子类也可以复写该方法，完成自己的初始化
     }
--- app/src/main/java/com/example/im/ui/fragment/ContactFragment.kt
+++ app/src/main/java/com/example/im/ui/fragment/ContactFragment.kt
@@ -9,11 +9,9 @@ import com.example.im.contract.ContactContract
 import com.example.im.presenter.ContactPresenter
 import com.example.im.ui.activity.AddFriendActivity
 import com.example.im.widget.SlideBar
-import com.hyphenate.EMContactListener
 import com.hyphenate.chat.EMClient
 import kotlinx.android.synthetic.main.fragment_contacts.*
 import kotlinx.android.synthetic.main.header.*
-import kotlinx.android.synthetic.main.view_contact_item.*
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
 
@@ -26,13 +24,34 @@ class ContactFragment :BaseFragment(),ContactContract.View{
 
     val presenter = ContactPresenter(this)
 
+    val contactListener = object : EMContactListenerAdapter(){
 
+        override fun onContactDeleted(p0: String?) {
+            //重新获取联系人的数据
+            presenter.loadContacts()
+
+        }
+
+
+        override fun onContactAdded(p0: String?) {
+            //重新获取联系人的数据
+            presenter.loadContacts()
+
+        }
+
+    }
 
     override fun onLoadContactSuccess() {
         swipeRefreshLayout.isRefreshing = false
         recyclerView.adapter?.notifyDataSetChanged()
 
 
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        EMClient.getInstance().contactManager().removeContactListener(contactListener)
     }
 
     override fun onLoadContactsFailed() {
@@ -46,61 +65,14 @@ class ContactFragment :BaseFragment(),ContactContract.View{
     override fun init() {
         super.init()
 
-        //添加好友
-        headerTitle.text = getString(R.string.contact)
-        add.visibility = View.VISIBLE
-        add.setOnClickListener { context?.startActivity<AddFriendActivity>() }
+        initHeader()
 
+        initSwipeRefreshLayout()
 
-        swipeRefreshLayout.apply {
-            setColorSchemeResources(R.color.qq_blue)
-            isRefreshing = true
+        initRecyclerView()
 
-            setOnRefreshListener { presenter.loadContacts() }
-        }
-
-        recyclerView.apply {
-            setHasFixedSize(true)
-            layoutManager = LinearLayoutManager(context)
-            adapter =  ContactListAdapter(context,presenter.contactListItems)
-
-
-        }
-
-
-        EMClient.getInstance().contactManager().setContactListener(object : EMContactListenerAdapter(){
-
-            override fun onContactDeleted(p0: String?) {
-                //重新获取联系人的数据
-                presenter.loadContacts()
-
-            }
-
-
-            override fun onContactAdded(p0: String?) {
-                //重新获取联系人的数据
-                presenter.loadContacts()
-
-            }
-
-        })
-
-
-        slideBar.onSectionChangeListener = object : SlideBar.OnSectionChangeListener{
-            override fun onSectionChange(firstLetter: String) {
-
-                section.visibility = View.VISIBLE
-                section.text = firstLetter
-                recyclerView.smoothScrollToPosition(getPosition(firstLetter))
-            }
-
-            override fun onSlideFinish() {
-                section.visibility = View.GONE
-
-            }
-
-        }
-
+        EMClient.getInstance().contactManager().setContactListener(contactListener)
+        initSlideBar()
         presenter.loadContacts()
 
     }
@@ -109,6 +81,52 @@ class ContactFragment :BaseFragment(),ContactContract.View{
         presenter.contactListItems.binarySearch {
             contactListItem -> contactListItem.firstLetter.minus(firstLetter[0])
         }
+
+
+    private fun initHeader(){
+        //添加好友
+        headerTitle.text = getString(R.string.contact)
+        add.visibility = View.VISIBLE
+        add.setOnClickListener { context?.startActivity<AddFriendActivity>() }
+    }
+
+   private fun initSwipeRefreshLayout(){
+        swipeRefreshLayout.apply {
+            setColorSchemeResources(R.color.qq_blue)
+            isRefreshing = true
+
+            setOnRefreshListener { presenter.loadContacts() }
+        }
+   }
+
+   private fun initRecyclerView() {
+        recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(context)
+            adapter = ContactListAdapter(context, presenter.contactListItems)
+
+
+        }
+   }
+
+   private fun initSlideBar(){
+       slideBar.onSectionChangeListener = object : SlideBar.OnSectionChangeListener{
+           override fun onSectionChange(firstLetter: String) {
+
+               section.visibility = View.VISIBLE
+               section.text = firstLetter
+               recyclerView.smoothScrollToPosition(getPosition(firstLetter))
+           }
+
+           override fun onSlideFinish() {
+               section.visibility = View.GONE
+
+           }
+
+       }
+   }
+
+
 
 
 
